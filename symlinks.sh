ec76169458e1d95e76a7adddbd0ba75a6220b7fd
Aidan Scannell <scannell.aidan@gmail.com>
2020-03-22 00:30:36 +0000

    Removed directory creation and now symlink folders instead.

--- symlinks.sh
+++ symlinks.sh
@@ -3,32 +3,25 @@ echo 'Symlinking dotfiles'
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.mbsyncrc ~/.mbsyncrc
-ln -s ~/.dotfiles/.spacemacs ~/.spacemacs
+#ln -s ~/.dotfiles/.spacemacs ~/.spacemacs
 ln -s ~/.dotfiles/.warprc ~/.warprc
 ln -s ~/.dotfiles/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 mkdir ~/.config
-mkdir ~/.config/htop
-mkdir ~/.config/msmtp
-mkdir ~/.config/mutt
-mkdir ~/.config/neofetch
-mkdir ~/.config/ranger
-mkdir ~/.config/skhd
-mkdir ~/.config/yabai
-mkdir ~/.config/zathura
 
-ln -s ~/.dotfiles/config/htop ~/.config
-ln -s ~/.dotfiles/config/msmtp ~/.config
-ln -s ~/.dotfiles/config/mutt ~/.config
-ln -s ~/.dotfiles/config/neofetch ~/.config
-ln -s ~/.dotfiles/config/ranger ~/.config
-ln -s ~/.dotfiles/config/skhd ~/.config
-ln -s ~/.dotfiles/config/yabai ~/.config
-ln -s ~/.dotfiles/config/zathura ~/.config
+ln -s ~/.dotfiles/config/htop ~/.config/
+ln -s ~/.dotfiles/config/msmtp ~/.config/
+ln -s ~/.dotfiles/config/mutt ~/.config/
+ln -s ~/.dotfiles/config/neofetch ~/.config/
+ln -s ~/.dotfiles/config/ranger ~/.config/
+ln -s ~/.dotfiles/config/skhd ~/.config/
+ln -s ~/.dotfiles/config/yabai ~/.config/
+ln -s ~/.dotfiles/config/zathura ~/.config/
 
 ln -s ~/.dotfiles/.emacs-profile ~/.emacs-profile
 ln -s ~/.dotfiles/.emacs-profiles.el ~/.emacs-profiles.el
 
-ln -s ~/.dotfiles/chemacs/.emacs ~/.emacs 
+ln -s ~/.dotfiles/chemacs/.emacs ~/.emacs
+
 echo 'done'
